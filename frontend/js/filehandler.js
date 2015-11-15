b1d556816d289a73494e6d442535af2fd6bc8ee2
liamrahav <liorogamerdvd@gmail.com>
2015-11-15 11:51:54 -0500

    okdoofodsfoks

--- frontend/js/filehandler.js
+++ frontend/js/filehandler.js
@@ -7,14 +7,14 @@ function displayFile(name) {
 ; This program draws my initial.  Pressing different numbers results in\n\
 ; the initials displaying with different colors.\n\
 ;\n\
-
+\
 define ASCII_0      $30	    ; Value for ASCII 0 in hex\n\
-
+\
 ; System variables\n\
 define sysLastKey   $ff\n\
 define current_char $12\n\
 define current_digit $13\n\
-
+\
 readKeys:\n\
 	lda sysLastKey\n\
 	sta current_char\n\
@@ -23,7 +23,7 @@ readKeys:\n\
 	sta current_digit\n\
 	jmp drawInitials\n\
 	jmp readKeys\n\
-
+\
 drawInitials:\n\
 	; Left most vertical bar\n\
 	STA $0264\n\
@@ -36,46 +36,46 @@ drawInitials:\n\
 	STA $0344\n\
 	STA $0364\n\
 	STA $0384\n\
-
+\
 	; Top Horizontal Section\n\
 	STA $0265\n\
 	STA $0266\n\
 	STA $0267\n\
 	STA $0268\n\
-
+\
 	; Right vertical bar\n\
 	STA $0289\n\
 	STA $02A9\n\
 	STA $02C9\n\
 	STA $02E9\n\
-
+\
 	; Bottom Horizontal Section\n\
 	STA $0308\n\
 	STA $0307\n\
 	STA $0306\n\
 	STA $0305\n\
-
+\
 	; Tail\n\
 	STA $0326\n\
 	STA $0347\n\
 	STA $0368\n\
 	STA $0389\n\
-
-	jmp readKeys
+\
+	jmp readKeys\
 ";
 program['disassembled'] = "\n\
 ;\n\
 ; This program draws my initial.  Pressing different numbers results in\n\
 ; the initials displaying with different colors.\n\
 ;\n\
-
+\
 define ASCII_0      $30	    ; Value for ASCII 0 in hex\n\
-
+\
 ; System variables\n\
 define sysLastKey   $ff\n\
 define current_char $12\n\
 define current_digit $13\n\
-
+\
 readKeys:\n\
 	lda sysLastKey\n\
 	sta current_char\n\
@@ -84,7 +84,7 @@ readKeys:\n\
 	sta current_digit\n\
 	jmp drawInitials\n\
 	jmp readKeys\n\
-
+\
 drawInitials:\n\
 	; Left most vertical bar\n\
 	STA $0264\n\
@@ -97,31 +97,31 @@ drawInitials:\n\
 	STA $0344\n\
 	STA $0364\n\
 	STA $0384\n\
-
+\
 	; Top Horizontal Section\n\
 	STA $0265\n\
 	STA $0266\n\
 	STA $0267\n\
 	STA $0268\n\
-
+\
 	; Right vertical bar\n\
 	STA $0289\n\
 	STA $02A9\n\
 	STA $02C9\n\
 	STA $02E9\n\
-
+\
 	; Bottom Horizontal Section\n\
 	STA $0308\n\
 	STA $0307\n\
 	STA $0306\n\
 	STA $0305\n\
-
+\
 	; Tail\n\
 	STA $0326\n\
 	STA $0347\n\
 	STA $0368\n\
 	STA $0389\n\
-
+\
 	jmp readKeys\n\
 ";
 
@@ -133,146 +133,113 @@ STA $0201\n\
 LDA #$08\n\
 STA $0202\n\
 "
-program['functions'] = "
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Constant values
-;
-define x 	   $02		; Input "x" parameter
-define y	   $04		; Input "y" parameter
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Addresses
-;
-define ans_addr	   $01   	; Final answer will be stored at $0001. 
-define temp_addr   $02		; Temporary values can be stored at $0002.
-define temp_addr_2   $04    ; Temporary values can also be stored at $0004.
+program['functions'] = "\
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n\
+; Constant values\n\
+;\n\
+define x 	   $02		; Input \"x\" parameter\n\
+define y	   $04		; Input \"y\" parameter\n\
+\n\
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n\
+; Addresses\n\
+;\n\
+define ans_addr	   $01   	; Final answer will be stored at $0001. \n\
+define temp_addr   $02		; Temporary values can be stored at $0002.\n\
+define temp_addr_2   $04    ; Temporary values can also be stored at $0004.\n\
+\n\
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n\
+; Main Program\n\
+; -- Push x then y onto the stack\n\
+; -- Call function e\n\
+; -- The pop the result from the stack and store it in ans_addr\n\
+LDA #x\n\
+PHA \n\
+LDA #y\n\
+PHA \n\
+JSR e\n\
+PLA ; get result\n\
+STA ans_addr\n\
+PLA ; throw away x\n\
+BRK\n\
+\n\
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n\
+; e(x,y) = a(d(x+1)) + d(y)\n\
+; \n\
+e: \n\
+;\n\
+; Implement this!\n\
+;\n\
+\n\
+\n\
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n\
+; d(x) = 5x + 7  \n\
+;\n\
+d: \n\
+;\n\
+; Implement this!\n\
+;\n\
+\n\
+\n\
+	\n\
+\n\
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n\
+; a(x) = 2x + 3\n\
+;\n\
+a:\n\
+	;\n\
+	; Load the \"x\" parameter from SP + 3\n\
+	TSX\n\
+	TXA\n\
+	CLC\n\
+	ADC #$03\n\
+	TAY\n\
+	LDA $0100, Y\n\
+\n\
+	; Store \"x\" to a temp location, then add it back to the accumulator.\n\
+	; This effectively gives us 2x\n\
+	STA temp_addr\n\
+	ADC temp_addr\n\
+\n\
+	; Add 3 to the accumulator, resulting in x+3\n\
+	ADC #$03\n\
+\n\
+	; Store the result back on the stack and return.\n\
+	STA $0100, Y\n\
+	RTS\n\
+\n\
+\n\
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n\
+; b(x) = a(x+2) + a(x+1)\n\
+;\n\
+b:\n\
+	;\n\
+	; Load the \"x\" parameter from SP + 3\n\
+	TSX\n\
+	TXA\n\
+	CLC\n\
+	ADC #$03\n\
+	TAY\n\
+	LDA $0100, Y\n\
+\n\
+	; Add 1 to the accumulator to get x + 1\n\
+	; Push x+1 to the stack as an argument for \"a\".\n\
+	ADC #$01	\n\
+	PHA \n\
+	JSR a\n\
+\n\
+	; Leave the result of a(x+1) on the stack for later\n\
+\n\
+	;\n\
+	; Load the \"x\" parameter from SP + 4\n\
+	; Why is it now at SP + 4?  Because we left a(x+1) on the stack!\n\
+	;\n\
+	TSX\n\
+	TXA\n\
+	CLC\n\
+	ADC #$04\n\
+	TAY\n\
+	LDA $0100, Y \n";	
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Main Program
-; -- Push x then y onto the stack
-; -- Call function e
-; -- The pop the result from the stack and store it in ans_addr
-LDA #x
-PHA 
-LDA #y
-PHA 
-JSR e
-PLA ; get result
-STA ans_addr
-PLA ; throw away x
-BRK
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; e(x,y) = a(d(x+1)) + d(y)
-; 
-e: 
-;
-; Implement this!
-;
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; d(x) = 5x + 7  
-;
-d: 
-;
-; Implement this!
-;
-
-
-	
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; a(x) = 2x + 3
-;
-a:
-	;
-	; Load the "x" parameter from SP + 3
-	TSX
-	TXA
-	CLC
-	ADC #$03
-	TAY
-	LDA $0100, Y
-
-	; Store "x" to a temp location, then add it back to the accumulator.
-	; This effectively gives us 2x
-	STA temp_addr
-	ADC temp_addr
-
-	; Add 3 to the accumulator, resulting in x+3
-	ADC #$03
-
-	; Store the result back on the stack and return.
-	STA $0100, Y
-	RTS
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; b(x) = a(x+2) + a(x+1)
-;
-b:
-	;
-	; Load the "x" parameter from SP + 3
-	TSX
-	TXA
-	CLC
-	ADC #$03
-	TAY
-	LDA $0100, Y
-
-	; Add 1 to the accumulator to get x + 1
-	; Push x+1 to the stack as an argument for "a".
-	ADC #$01	
-	PHA 
-	JSR a
-
-	; Leave the result of a(x+1) on the stack for later
-
-	;
-	; Load the "x" parameter from SP + 4
-	; Why is it now at SP + 4?  Because we left a(x+1) on the stack!
-	;
-	TSX
-	TXA
-	CLC
-	ADC #$04
-	TAY
-	LDA $0100, Y
-
-		
-	; Add 2 to the accumulator to get x + 2
-	; Push x+2 to the stack as an argument for "a".
-	ADC #$02   
-	PHA 
-	JSR a
-
-	;
-	; Pop a(x+2) from the stack and store it in a temp location
-	;	
-	PLA				
-	STA temp_addr   
-
-	;
-	; Now pop a(x+1) from the stack.  Then add a(x+2) to this value.
-	PLA				
-	ADC temp_addr
-
-	; Save the final value in a temp address while we calculate where to store it on the stack.
-	STA temp_addr
-
-	; The return location is SP + 3
-	TSX
-	TXA
-	CLC
-	ADC #$03
-	TAY
-
-	; Load the final value from the temp address and write it to the stack.
-	LDA temp_addr
-	STA $0100, Y
-
-	RTS
-"
 
 }
